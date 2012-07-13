c07bb7451f6f3611b2d8a1bd8b9c6550a7cefd02
Tony Rieker <tony@plenary.org>
2012-07-12 23:53:42 -0700

    calendar updated to 2012

--- calendar.php
+++ calendar.php
@@ -2,62 +2,92 @@
 include('includes/header.php');
 ?>
 
-<div class="pageHeader">BRC 2011 :: VW Bus Camp's Calendar of Events</div>
+<div class="pageHeader">BRC 2012 :: VW Bus Camp's Calendar of Events</div>
 
 <div class="header">Monday</div>
 <div class="contentSection">
     <div class="content">
-        Leopard Lounge blissful massaging and tequila shots!
+        <strong>All Day</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/7086/" target="_blank">Leopard Lounge Day Spa &amp; Tequila Bar</a> - Our 5th year on the playa and a very popular daytime destination enjoyed by all who find us. Each year, we do nothing more than create a fun jungle-vibe space where you can get your feet (and body should you desire) rubbed, and share a shot of tequila with us. The Leopard Lounge runs on the kindness of strangers, so we also welcome you to join our staff and participate in the gifting of a footrub or massage, just be sure to do so with respect to personal boundaries. If you have other gifts to share: food, musical talent, energy work, manicure/pedicure, hair braiding, readings, etc., the Leopard Lounge is a great place to park yourself and offer your services. No scheduling or expectations, just gratitude for however long you want to contribute. The vibe is all things Jungle, Safari, Animal, so dress the part if you can!
+    </div>
+    <div class="content">
+        <strong>8:30AM - 10AM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6530/" target="_blank">Fountain of Youth</a> - Start your day by practicing the Five Tibetan Rites and/or yoga with us! The 2,500 year-old Tibetan Rites are said to be the "Fountain of Youth", as they wake up and energize not only your muscles but also your endocrine glands, psychic vortexes, or chakras. After the Rites (20-30 minutes), we will continue with a traditional yoga practice, ranging from 30-60 minutes. Bring a mat or towel!
+    </div>
+    <div class="content">
+        <strong>10AM - 2PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6311/" target="_blank">Burners Without Matches</a> - Sit down, burn a wood medallion with a magnifying glass! This is DIY gifting!
     </div>
 </div>
 
 <div class="header">Tuesday</div>
 <div class="contentSection">
     <div class="content">
-        <strong>Sometime - Dark</strong>: Leopard Lounge blissful massaging and tequila shots!
+        <strong>All Day</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/7086/" target="_blank">Leopard Lounge Day Spa &amp; Tequila Bar</a>
     </div>
     <div class="content">
-        <strong>Noonish</strong>: Bus Camp Hoop Jam!
+        <strong>8:30AM - 10AM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6530/" target="_blank">Fountain of Youth</a>
+    </div>
+    <div class="content">
+        <strong>10AM - 2PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6311/" target="_blank">Burners Without Matches</a>
     </div>
 </div>
 
 <div class="header">Wednesday</div>
 <div class="contentSection">
     <div class="content">
-        <strong>Sometime - Dark</strong>: Leopard Lounge blissful massaging and tequila shots!
+        <strong>All Day</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/7086/" target="_blank">Leopard Lounge Day Spa &amp; Tequila Bar</a>
     </div>
     <div class="content">
-        <strong>10AM - 2PM</strong>: Grateful Dye - Please bring something 100% cotton or silk prewashed to get your dye on with.
+        <strong>8:30AM - 10AM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6530/" target="_blank">Fountain of Youth</a>
+    </div>
+    <div class="content">
+        <strong>10AM - 2PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6311/" target="_blank">Burners Without Matches</a>
+    </div>
+    <div class="content">
+        <strong>11AM - 3PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6520/" target="_blank">Grateful Dyed</a> - Join us at VW Bus Camp for a Tie-dye workshop. This is a moop free project. Bring a clean dry 100% cotton item and we will have all the rest and some great music too..
     </div>
 </div>
 
 <div class="header">Thursday</div>
 <div class="contentSection">
     <div class="content">
-        <strong>Sometime - Dark</strong>: Leopard Lounge blissful massaging and tequila shots!
+        <strong>All Day</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/7086/" target="_blank">Leopard Lounge Day Spa &amp; Tequila Bar</a>
     </div>
+    <div class="content">
+        <strong>8:30AM - 10AM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6530/" target="_blank">Fountain of Youth</a>
+    </div>
+    <div class="content">
+        <strong>10AM - 2PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6311/" target="_blank">Burners Without Matches</a>
+    </div>
+    <div class="content">
+        <strong>11AM - 1PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6540/" target="_blank">Cure Menstrual Cramps</a> - Every woman should know this simple shiatsu trick to alleviate monthly cramps. Come and learn. Teach your friends! Bring a partner, bring your cramps; tho neither is required.
+    </div>
+    <div class="content">
+        <strong>1PM - 3PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6529/" target="_blank">Love Hoopjam</a> - VW Bus Camp LOVEs hoopers! All levels are LOVEingly welcomed to this unique hoop-ening! All celestial signs are pointing to a global shift in human potential for LOVE, so this year's hoopjam theme is "All LOVE, All the Time"! With the incredible sound system provided by Walter, The World's Largest VW Bus, we will spin and groove to an entire playlist full of, you guessed it, LOVE songs! Stop by our Leopard Lounge before or after the jam to get to know and LOVE our wonderful campmates! RED is THE color to wear this day, but we don't mind if you come just as you are - we LOVE you! XOXO
+    </div>
+
 </div>
 
 <div class="header">Friday</div>
 <div class="contentSection">
     <div class="content">
-        <strong>Sometime - Dark</strong>: Leopard Lounge blissful massaging and tequila shots!
+        <strong>All Day</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/7086/" target="_blank">Leopard Lounge Day Spa &amp; Tequila Bar</a>
     </div>
+    <div class="content">
+        <strong>8:30AM - 10AM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6530/" target="_blank">Fountain of Youth</a>
+    </div>
+    <div class="content">
+        <strong>10AM - 2PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6311/" target="_blank">Burners Without Matches</a>
+    </div>
+
 </div>
 
 <div class="header">Saturday</div>
 <div class="contentSection">
     <div class="content">
-        <strong>Sometime - Dark</strong>: Leopard Lounge blissful massaging and tequila shots!
+        <strong>10AM - 2PM</strong>: <a href="http://playaevents.burningman.com/2012/playa_event/6311/" target="_blank">Burners Without Matches</a>
     </div>
+
 </div>
 
-<div class="header">Sunday</div>
-<div class="contentSection">
-    <div class="content">
-        ?
-    </div>
-</div>
 
 <?
 include('includes/footer.php');

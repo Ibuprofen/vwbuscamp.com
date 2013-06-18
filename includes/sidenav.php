ee47f0f2afb56da53c3f7bb255b175c7e86777d2
Tony Rieker <tony@plenary.org>
2013-06-18 08:24:36 -0700

    hiding sidenav camp location

--- includes/sidenav.php
+++ includes/sidenav.php
@@ -1,10 +1,10 @@
 <div class="sidebaritem">
     <img src="images/vwbuscamp_logo.png" alt="VWBusCamp"/>
 </div>
-<div class="sidebaritem" style="margin-top: 12px;">
+<!--<div class="sidebaritem" style="margin-top: 12px;">
     <strong>Our new home!</strong>
     <div style="text-align:center">Edelweiss &amp; 3:15</div>
-</div>
+</div>-->
 <div class="sidebaritem">
     <a href="discussionlist.php">Click here to join the discussion list!</a>
 </div>
